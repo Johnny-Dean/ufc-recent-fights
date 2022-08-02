bb548b939ec78213680e252477f8eb2b6cffda50
Johnny-Dean <73153392+Johnny-Dean@users.noreply.github.com>
2022-08-01 22:26:28 -0700

    Build with proper url

--- client/lib/apollo.ts
+++ client/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "/4000",
+  uri: "http://mma-fights-backend.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
